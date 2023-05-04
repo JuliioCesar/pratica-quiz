096ab38566f1bffad9c8ff2ea13c7ac0f6deb87d
Julio Cesar <64658627+JuliioCesar@users.noreply.github.com>
2023-05-04 00:32:16 -0300

    adicionando uma lista de questoes

---
+++ script/listQuestions.js
@@ -0,0 +1,44 @@
+export const questionsForQuiz = [
+  {
+    question:
+      "Qual das seguintes linguagens é utilizada para estilizar páginas web?",
+    options: ["JavaScript",
+      "Python", "C++", "Css"],
+    answer: "Css",
+  },
+  {
+    question:
+      "Qual das seguintes ferramentas é usada para desenvolver componentes reutilizáveis em React?",
+    options: ["Angular", "Vue", "Hooks", "Webpack"],
+    answer: "Hooks",
+  },
+  {
+    question:
+      "Qual das seguintes bibliotecas JavaScript é comumente usada para criar animações em páginas web?",
+    options: ["Jquery", "Lodash", "Greensock", "D3.js"],
+    answer: "Greensock",
+  },
+  {
+    question:
+      "Qual das seguintes tags HTML é usada para exibir links em uma página web? ",
+    options: ["<a>", "<img>", "<div>", "<p>"],
+    answer: "<a>",
+  },
+  {
+    question:
+      "Qual das seguintes ferramentas é usada para rastrear e resolver problemas de performance em uma página web?",
+    options: ["Chrome DevTools", "Firebase", "AWS", "Azure"],
+    answer: "Chrome DevTools",
+  },
+  {
+    question:
+      "Qual das seguintes técnicas é usada para tornar uma página web mais acessível a usuários com necessidades especiais?",
+    options: [
+      "Responsive Design",
+      "Progressive Web App",
+      "Accessebility",
+      "Serve-side rendering",
+    ],
+    answer: "Accessibility",
+  },
+];
